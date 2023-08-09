efa131647604f156a2709c2b9c2433a13a610098
Mariam Kamel <mariammkamel98@gmail.com>
2023-08-09 23:52:51 +0300

    deploy backend

--- index.py
+++
@@ -1,7 +0,0 @@
-from langchain.llms import OpenAI
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks import StreamlitCallbackHandler
-import streamlit as st
-import os
-
-st_callback = StreamlitCallbackHandler(st.container())
